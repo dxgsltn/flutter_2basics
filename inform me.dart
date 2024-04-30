1a8a3554f2c81826fc2544c1c317ff1c3c0117ff
dxgsltn <163827345+dxgsltn@users.noreply.github.com>
2024-04-30 19:45:54 +0500

    Add files via upload

---
+++ inform me.dart
@@ -0,0 +1,24 @@
+void main() {
+  print('Hello world: !');
+  hello();
+  printName();
+  documentation();
+}
+
+/// this is hello method, do yourself
+void hello() {
+  print("Hello");
+}
+
+/// methods for printing name
+void printName() {
+  print("My name is Gusha and I live in Taraz");
+}
+
+/**
+* this type of comment for documentation
+*/
+void documentation() {
+  print(
+      "I am a student of JIHC college. I like music, singing, food and my mom).");
+}
